f15e91377e33597e7c6a6600746e043a726fd21e
Tanishq Motke <tanishq@xecurify.com>
2024-02-22 15:59:11 +0530

    latest code from Local Machine

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,2 +1,21 @@
 console.log(typeof undefined)
 console.log(typeof null)
+
+/* 
+rimitive DataTypes
+Number 
+bigint
+String =>” “
+Boolean => true/false
+null => standalone value (empty)
+Undefined => when the value is not assigned.
+Symbol => unique
+
+Typeof: used to know what is the type of that variable.
+
+Example : console.log(typeof “Hitesh”) 
+Output: string
+
+Typeof null is object and typeof undefined is undefined.
+
+*/
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "33"
+console.log(typeof score); //console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(score)
+console.log("------------")
+
+let score1="33Asdas"
+let valueInNumber1 = Number(score1)
+console.log(valueInNumber1)
+console.log(typeof valueInNumber1)
+console.log("------------")
+
+//When you convert the values of score to Number you will get the below results
+//  "33" => 33
+//  "33abc" => NaN
+//  "tanu" =>NaN
+//  null => 0
+//  true => 1
+//  undefined => NaN
+
+let IsLogged = 1;
+let converToBollean = Boolean(IsLogged)
+console.log(typeof converToBollean)
+console.log(converToBollean)
+console.log("------------")
+//  1 => true
+//  0 => false
+//  "tanu" =>true
+//   "" => false
+
+let someNumber = 33
+let ConvertToString = String(someNumber)
+console.log(typeof ConvertToString)
+console.log(ConvertToString)
